b024d40069cef3fcd3c3d9cc2306ec8fba5b4a6b
Aspect13 <ad13box@gmail.com>
2023-07-21 16:46:14 +0400

    logging possible error

--- control_tower/run.py
+++ control_tower/run.py
@@ -23,6 +23,7 @@ from datetime import datetime
 from json import dumps
 from time import sleep, time
 import xml.etree.ElementTree as et
+from traceback import format_exc
 
 import arbiter
 import requests
@@ -755,6 +756,7 @@ def start_and_track(args=None):
         _start_and_track(args)
     except:
         status_code = 500
+        logger.error(format_exc())
         raise
     finally:
         send_minio_dump_flag(status_code)
